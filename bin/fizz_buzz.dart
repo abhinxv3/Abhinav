c73581c94ac4d6e26a1a447c0b7ec695d5a9c5af
abhinxv3 <abhinavas323@gmail.com>
2024-08-23 13:11:54 +0530

    fizz buzz

--- bin/fizz_buzz.dart
+++ bin/fizz_buzz.dart
@@ -4,9 +4,22 @@ void main()
 {
   print("Enter a Number:");
   var a = int.parse(stdin.readLineSync()!);
-  for(int i=0;i<=a;i+=2)
+  for(int i=1;i<=a;i++)
   {
-    print(i);
-
+     if(i%3==0 && i%5==0)
+     {
+       print("Fizz Buzz");
+     }
+     else if(i%3==0)
+     {
+       print("Fizz");
+     }
+     else if(i%5==0)
+     {
+       print("Buzz");
+     }
+     else {
+       print(i);
+     }
   }
 }
