f500d2115b034bfbe501de2e8d6847998cffc1c2
abhinxv3 <abhinavas323@gmail.com>
2024-08-26 11:00:02 +0530

    Grade Of a Students

--- bin/Grade.dart
+++ bin/Grade.dart
@@ -13,36 +13,36 @@ void main()
     print("Grade: A+");
   }
 
-  else if(a>=81 && a<=90)
+  else if(a>=81)
   {
     print("Grade: A");
   }
 
-  else if(a>=71 && a<=80)
+  else if(a>=71)
   {
     print("Grade: B+");
   }
 
-  else if(a>=61 && a<=70)
+  else if(a>=61)
   {
     print("Grade: B");
   }
 
-  else if(a>=51 && a<=60)
+  else if(a>=51)
   {
     print("Grade: C+");
   }
-  else if(a>=41 && a<=50)
+  else if(a>=41)
   {
     print("Grade: C");
   }
 
-  else if(a>=33 && a<=40)
+  else if(a>=33)
   {
     print("Grade: D+");
   }
 
-  else if(a>=21 && a<=32)
+  else if(a>=21)
   {
     print("Grade: D");
   }
